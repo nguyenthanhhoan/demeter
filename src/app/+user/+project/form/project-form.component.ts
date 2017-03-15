6ab1538cc389a2c85e373aca626a68701058f138
Su Tran <trannguyentiensu@gmail.com>
2017-03-15 23:22:42 +0800

    finish UI for creating project

--- src/app/+user/+project/form/project-form.component.ts
+++ src/app/+user/+project/form/project-form.component.ts
@@ -67,18 +67,9 @@ export class ProjectFormComponent implements OnInit {
       // For each place, get the icon, name and location.
       var bounds = new google.maps.LatLngBounds();
       places.forEach(function(place) {
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
         // Create a marker for each place.
         markers.push(new google.maps.Marker({
           map: map,
-          icon: icon,
           title: place.name,
           position: place.geometry.location
         }));
