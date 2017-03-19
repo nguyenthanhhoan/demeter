568766fcf91479f7187a8c57d612151a4b74b205
Su Tran <trannguyentiensu@gmail.com>
2017-03-19 16:37:57 +0800

    - icon for navigation menu - implement back button

--- src/app/+user/shared/layout/header/header.component.ts
+++ src/app/+user/shared/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
 import {Router} from "@angular/router";
 
 import { LocalStorageService } from '../../../../shared/utils/localstorage.service';
@@ -13,7 +14,8 @@ declare var $: any;
 export class HeaderComponent implements OnInit {
 
   constructor(private localStorageService: LocalStorageService,
-              private router: Router) {
+              private router: Router,
+              private location: Location) {
   }
 
   ngOnInit() {
@@ -23,6 +25,10 @@ export class HeaderComponent implements OnInit {
   logout(){
     let user = this.localStorageService.remove('user');
     this.router.navigate(['/auth/login']);
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
