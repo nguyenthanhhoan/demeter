18e4a75a77cd40279df32c86292a6fc1a5163f33
Su Tran <trannguyentiensu@gmail.com>
2017-06-05 21:36:49 +0800

    new ui for login page

--- src/app/+auth/+login/login.component.ts
+++ src/app/+auth/+login/login.component.ts
@@ -11,7 +11,8 @@ class User {
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
