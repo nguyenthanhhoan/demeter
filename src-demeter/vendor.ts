50258ea484feba21ca61ba22937e11db84701de5
Su Tran <trannguyentiensu@gmail.com>
2017-09-29 22:50:17 +0700

    Install summernote, codemirror

--- src-demeter/vendor.ts
+++ src-demeter/vendor.ts
@@ -6,6 +6,10 @@ window['$'] = window['jQuery'];
 window['moment'] = require('moment');
 window['Noty'] = require('noty/lib/noty.min.js');
 window['uuid'] = require('uuid/v4');
+require('bootstrap/js/tooltip.js'); // required for summernote
+require('bootstrap/js/modal.js'); // required for summernote
+require('codemirror');
+require('summernote/dist/summernote.js');
 require('mediaelement/build/mediaelement-and-player.js');
 require('moment-timezone');
 require('jquery-confirm');
