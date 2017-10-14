a43cb3f4f25d9e6f2ae8ede23e32c551a9ea3e8f
Su Tran <trannguyentiensu@gmail.com>
2017-10-14 10:09:29 +0800

    - entry.sh for dmt-corp, dmt-admin - update docker compose file

---
+++ src-admin/entry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /rails/src-admin
+# npm run build:prod
+cd dist
+static-server -p 4202
---
+++ src-corp/entry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /rails/src-corp
+# npm run build:prod
+cd dist
+static-server -p 4201
