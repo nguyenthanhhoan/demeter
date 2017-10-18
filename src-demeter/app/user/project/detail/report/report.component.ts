37d7f2d4c082024132c025f7c8a55e68e08ee884
haintwork <haint.work@gmail.com>
2017-10-18 23:16:36 +0700

    complete UI desktop mode for Report screen

--- src-demeter/app/user/project/detail/report/report.component.ts
+++ src-demeter/app/user/project/detail/report/report.component.ts
@@ -1,13 +1,21 @@
 import { ISubscription } from 'rxjs/Subscription';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { AppSettings } from '../../../../app.settings';
 
+declare var moment: any;
 @Component({
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit, OnDestroy {
+  isRequesting: boolean = false;
   project: any = {};
+  reportFilter: any = {
+    date: moment().format(AppSettings.date_time_format.date_iso),
+    start_time: '00:00',
+    end_time: '23:59'
+  };
   private storeSubscription: ISubscription;
   constructor(private store: Store<any>) {}
 
@@ -22,5 +30,9 @@ export class ReportComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.storeSubscription.unsubscribe();
+  }
+
+  report() {
+    console.log('Report Button clicked!');
   }
 }
