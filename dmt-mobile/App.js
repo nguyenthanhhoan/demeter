ad3506efdd590d5eb84a91b1320ac343d7275af6
Su Tran <trannguyentiensu@gmail.com>
2017-11-10 22:07:57 +0800

    Use WebView to render on mobile app

--- dmt-mobile/App.js
+++ dmt-mobile/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, WebView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <WebView
+        source={{uri: 'http://family.demeter.vn'}}
+        style={{marginTop: 20}}
+      />
     );
   }
 }
@@ -16,7 +15,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
